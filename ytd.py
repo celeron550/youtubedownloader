4ab76133c4cad15054b3bc7cfe903d4a1af64d53
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2024-01-05 16:59:00 -0300

    Update ytd.py
    minor optimizations

--- ytd.py
+++ ytd.py
@@ -1,55 +1,32 @@
 from pytube import YouTube, Playlist
-from pytube.exceptions import VideoUnavailable
 from os import rename
-
-def baixar(url):
-    global link,pl,ext
-    
+def baixar(link):
+    global ext
     try:
         if ext == '1':
             stream = link.streams.get_highest_resolution()
             stream.download(output_path="./downloads/")
+            print('Baixado.')
         elif ext == '2':
             stream = link.streams.get_lowest_resolution()
-            stream.download(output_path="./downloads/")
+            stream.download(output_path="./downloads/",)
+            print('Baixado.')
         elif ext == '3':
             stream = link.streams.get_audio_only()
-            stream.download(output_path="./downloads/")
+            stream.download(output_path="./downloads/",)
+            print('Baixado.')
             print('convertendo...')
-            file_path = f'./downloads/{link.title}.mp4'
-            rename(file_path, f'{link.title}.mp3')
-
-            
-        
-        
-        print('Baixado.')
-    except VideoUnavailable:
-        print(f'Vídeo indisponível:{link.title}')
-
-
-    
-
-isplaylist = input("é playlist \n[1] Sim [2] Não : ")
-if isplaylist == '1':
-    pl = Playlist(input('Link da playlist: '))
-    ext = input('Selecione como deseja baixar:\n[1]Resolução mais alta\n[2]Resolução mais baixa\n[3]Apenas áudio\n -> ')
-    for link in pl.videos:
-        print(f'Baixando Vídeo {link.title}')
-        baixar(pl)
-
-elif isplaylist == '2': 
-    link = YouTube(input('Link do Vídeo: '),
-        use_oauth=False,
-        allow_oauth_cache=True) 
-    ext = input('Selecione como deseja baixar\n[1]Resolução mais alta\n[2]Resolução mais baixa\n[3]Apenas áudio\n -> ') 
-    print(f'Baixando Vídeo: {link.title}')
-    baixar(link)
-    
-    
-else:
-    print('Resposta inválida, tente novamente') 
-    isplaylist = input("é playlist \n[1] Sim [2] Não : ")
-
-
-
-
+            nome_arquivo = str(link.title).replace(".","")
+            nome_arquivo = nome_arquivo.replace(",","")
+            nome_arquivo = nome_arquivo.replace("/","")
+            nome_arquivo = nome_arquivo.replace("-","")
+            nome_arquivo = nome_arquivo.replace("'","")
+            titulo = str(link.title).replace("/","")
+            file_path = f'./downloads/{nome_arquivo}.mp4'
+            rename(file_path, f'./downloads/{titulo}.mp3/')
+            print('concluído') 
+        else:
+            print('Resposta inválida, tente novamente')
+            ext = input('Selecione como deseja baixar\n[1]Resolução mais alta\n[2]Resolução mais baixa\n[3]Apenas áudio\n -> ')
+    except:
+        print('Vídeo indisponível :( ')
